ba2663a06677f6f8ffb526ce7940bce234a5ae6d
Iakuro-wc <wcdevcontent@gmail.com>
2019-07-13 12:42:09 -0700

    Updates

---
+++ precache-manifest.5f2f20c0282d62b7b16cf8cd377c4d96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a30494d81441ffac00100d2efe2e9d4",
+    "url": "/robodex/index.html"
+  },
+  {
+    "revision": "ce730c4ca881852746f4",
+    "url": "/robodex/static/css/main.d81f26c9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/robodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ce730c4ca881852746f4",
+    "url": "/robodex/static/js/main.bac4fb97.chunk.js"
+  },
+  {
+    "revision": "380b4856ec949f0a5d6e",
+    "url": "/robodex/static/js/runtime~main.7effc0b6.js"
+  }
+]);
